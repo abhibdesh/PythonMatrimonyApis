b21fc4f11302496e4870f7ae87f7cc2135287786
Abhilasha Deshmukh <abhibdesh@gmail.com>
2025-01-02 21:24:01 +0530

    new test

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ from jwt.exceptions import PyJWTError as DecodeError
 app = Flask(__name__)
 api = Api(app)
 CORS(app
-     , resources={r"/*": {
-        "origins": "http://localhost:5173",
-                          "allow_headers": "Authorization"
-                          }
-                  }
+    #  , resources={r"/*": {
+    #     "origins": "http://localhost:5173"
+    #                      , "allow_headers": "Authorization"
+    #                       }
+    #               }
      )
 service_account_key = './Config/FirebaseCreds.json'
 with open('./Config/Creds.json') as f:
@@ -41,12 +41,12 @@ class HelloWorld(Resource):
             print(e)
             return {"data": "HelloWorld!!!"}
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:5173')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', 'http://localhost:5173')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+#     return response
 
 api.add_resource(HelloWorld, '/HelloWorld')
 api.add_resource(UserLogin, '/UserLogin')
