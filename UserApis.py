4e4e04b464f400a96b0d10287ab271ac9cc85e4f
Abhilasha Deshmukh <abhibdesh@gmail.com>
2025-01-12 18:05:01 +0530

    Update UserApis.py

--- UserApis.py
+++ UserApis.py
@@ -216,7 +216,7 @@ class AddNewUser(Resource):
         
 
 class LogoutUser(Resource):
-    @jwt_required()
+    # @jwt_required()
     def post(self):
         try:
             print("Request Headers:", request.headers)
