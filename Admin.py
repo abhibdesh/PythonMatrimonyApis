d637face7cb05e73159936057e34a0feb7db08c0
Abhilasha Deshmukh <88450158+abhibdesh@users.noreply.github.com>
2025-03-12 13:06:46 +0530

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -114,169 +114,176 @@ class FetchAllUsersAdmin(Resource):
         page = int(request.json['pageNumber'])
         rowsPerPage = int(request.json['rowsPerPage'])
         Userid = request.json["Userid"]
-        
-        projection = {"_id": 0, "UserPassword": 0}
-        if not isPaidUser:
-            projection.update({"UserEmail": 0, "PhoneNumber": 0})
-        print("asdasd")
-        finaldataList = []
-
-        # Final Filters List 
-        allLocalities =[]
-        allIncomes =[]
-        allEducations = []
-
-        allBloodGroups = []
-        allFamilyTypes = []
-        allRaas = []
-        allNaadi = []
-        selectedHeight = 0
-        expectedAgeGapMin = 0
-        expectedAgeGapMax = 0
-
+        userCollection = db.get_collection("User");
+        cu = userCollection.find_one({"UserId": int(Userid)})
         try:
-            filters["IsDeleted"] = False
-            collection = db.get_collection('User')
-            curUser = collection.find_one({"UserEmail":current_user})
-            if(curUser["isLoggedIn"] == 0):
-                return jsonify({"message": "Failure", "error": "Something Went Wrong"})
-            currentUser = collection.find_one({"UserId": Userid}, projection)
-            if not currentUser:
-                return jsonify({"message": "User not found", "users": []})
+            if cu["UserRole"] == "3":
+                adminMapp = db.get_collection("AdminMapping")
+                d = adminMapp.find_one({"AdminEmail":currentUser})
+                data = collection.find({"ReferenceCode":d["ReferenceCode"],"UserRole":"2"},{"image":0, "_id":0, "UserPassword":0,"access_token":0})
+                print("asd")
+                for i in data:
+                    finaldataList.append(i)
+            else:
+                print("asdasd")
+                finaldataList = []
 
-            newFilter = {"UserId": {"$ne": Userid}, "IsDeleted": False, "IsActive":True }
-            if int(filters["selectedFromHeight"]) > 0 :
-                newFilter["Height"] = {"$gte": int(filters["selectedFromHeight"])}
-            if int(filters["selectedToHeight"]) > 0 :
-                newFilter["Height"] = {"$lte": int(filters["selectedToHeight"])}
-            if int(filters["expectedAgeGapMin"]) > 0 and currentUser["expectedAgeGapMin"] > 0:
-                currentUserAge = float(currentUser["age"])
-                lessThanAge = currentUserAge - int(currentUser["expectedAgeGapMin"])
-                newFilter["age"] = {"$gte": lessThanAge}
-            if int(filters["expectedAgeGapMax"]) > 0 and currentUser["expectedAgeGapMin"] > 0:
-                currentUserAge = float(currentUser["expectedAgeGapMax"])
-                greterThanAge = currentUserAge + int(currentUser["expectedAgeGapMax"]) 
-                newFilter["age"] = {"$lte": greterThanAge}
-            # newFilter["age"] = {"$gte": lessThanAge, "$lte":greterThanAge}
+                # Final Filters List 
+                allLocalities =[]
+                allIncomes =[]
+                allEducations = []
 
+                allBloodGroups = []
+                allFamilyTypes = []
+                allRaas = []
+                allNaadi = []
+                selectedHeight = 0
+                expectedAgeGapMin = 0
+                expectedAgeGapMax = 0
 
-             
-            # Checking Incomes
-            if len(filters["selectedIncomes"]) > 0:
-                for i in filters["selectedIncomes"]:
-                    allIncomes.append(i)
-            elif len(currentUser["selectedIncome"]) > 0 :
-                for i in currentUser["selectedIncome"]:
-                    allIncomes.append(i)
-
-            # Checking Localities
-            if len(filters["selectedLocatities"]) > 0 :
-                for i in filters["selectedLocatities"]:
-                    allLocalities.append(i)
-            elif(len(currentUser["selectedLocatities"]) > 0):
-                for i in currentUser["selectedLocatities"]:
-                    allLocalities.append(i)
             
-            # Checking Educations
-            if len(filters["selectedEducations"]) > 0:
-                for i in filters["selectedEducations"]:
-                    allEducations.append(i)
-            elif len(currentUser["selectedEducations"]) > 0 :
-                for i in currentUser["selectedEducations"]:
-                    allEducations.append(i)
+                filters["IsDeleted"] = False
+                collection = db.get_collection('User')
+                curUser = collection.find_one({"UserEmail":current_user})
+                if(curUser["isLoggedIn"] == 0):
+                    return jsonify({"message": "Failure", "error": "Something Went Wrong"})
+                currentUser = collection.find_one({"UserId": Userid}, projection)
+                if not currentUser:
+                    return jsonify({"message": "User not found", "users": []})
 
-            # Checking Blood Group
-            if len(filters["selectedBloodGroups"]) > 0:
-                for i in filters["selectedBloodGroups"]:
-                    allBloodGroups.append(i)
-            elif len(currentUser["selectedBloodGroups"]) > 0:
-                for i in currentUser["selectedBloodGroups"]:
-                    allBloodGroups.append(i)
-
-            # Checking Family Type
-            if len(filters["FamilyType"]) > 0:
-                for i in filters["FamilyType"]:
-                    allFamilyTypes.append(i)
-            elif len(currentUser["selectedFamilyType"]) > 0:
-                print(currentUser["selectedFamilyType"])
-                for i in currentUser["selectedFamilyType"]:
-                    allFamilyTypes.append(i)
-
-            # Checking Raas
-            if len(filters["selectedRaas"]) > 0:
-                for i in filters["selectedRaas"]:
-                    allRaas.append(i)
-            elif len(currentUser["selectedRaas"]) > 0:
-                for i in currentUser["selectedRaas"]:
-                    allRaas.append(i)
+                newFilter = {"UserId": {"$ne": Userid}, "IsDeleted": False, "IsActive":True }
+                if int(filters["selectedFromHeight"]) > 0 :
+                    newFilter["Height"] = {"$gte": int(filters["selectedFromHeight"])}
+                if int(filters["selectedToHeight"]) > 0 :
+                    newFilter["Height"] = {"$lte": int(filters["selectedToHeight"])}
+                if int(filters["expectedAgeGapMin"]) > 0 and currentUser["expectedAgeGapMin"] > 0:
+                    currentUserAge = float(currentUser["age"])
+                    lessThanAge = currentUserAge - int(currentUser["expectedAgeGapMin"])
+                    newFilter["age"] = {"$gte": lessThanAge}
+                if int(filters["expectedAgeGapMax"]) > 0 and currentUser["expectedAgeGapMin"] > 0:
+                    currentUserAge = float(currentUser["expectedAgeGapMax"])
+                    greterThanAge = currentUserAge + int(currentUser["expectedAgeGapMax"]) 
+                    newFilter["age"] = {"$lte": greterThanAge}
+                # newFilter["age"] = {"$gte": lessThanAge, "$lte":greterThanAge}
 
 
-            if len(filters["selectedNaadi"]) > 0:
-                for i in filters["selectedNaadi"]:
-                    allNaadi.append(i)
-            if len(currentUser["selectedNaadi"]) > 0:
-                for i in currentUser["selectedNaadi"]:
-                    allNaadi.append(i)
+                
+                # Checking Incomes
+                if len(filters["selectedIncomes"]) > 0:
+                    for i in filters["selectedIncomes"]:
+                        allIncomes.append(i)
+                elif len(currentUser["selectedIncome"]) > 0 :
+                    for i in currentUser["selectedIncome"]:
+                        allIncomes.append(i)
 
-            if int(filters["selectedSiblingsCousinsUpto"]) > 0:
-                newFilter["Siblings"] = {"$lte": int(filters["selectedSiblingsCousinsUpto"])}
-           
-            # Making Filters
-            if(len(allIncomes) > 0):
-                newFilter["IncomeGroup"] = {"$in":allIncomes}
-            if(len(allLocalities) > 0):
-                newFilter["CurrentAddress"] = {"$in":allLocalities}
-            if len(allEducations) > 0 :
-                newFilter["DegDip"] = {"$in":allEducations}
-            if len(allBloodGroups) > 0:
-                newFilter["BloodGrp"] = {"$in":allBloodGroups}
-            if len(allFamilyTypes) > 0:
-                newFilter["FamilyType"] = {"$in":allFamilyTypes}
-            if len(allRaas) > 0 :
-                newFilter["Raas"] = {"$in": allRaas}
-            if len(allNaadi) > 0:
-                newFilter["Naadi"] =  {"$in": allNaadi}
+                # Checking Localities
+                if len(filters["selectedLocatities"]) > 0 :
+                    for i in filters["selectedLocatities"]:
+                        allLocalities.append(i)
+                elif(len(currentUser["selectedLocatities"]) > 0):
+                    for i in currentUser["selectedLocatities"]:
+                        allLocalities.append(i)
+                
+                # Checking Educations
+                if len(filters["selectedEducations"]) > 0:
+                    for i in filters["selectedEducations"]:
+                        allEducations.append(i)
+                elif len(currentUser["selectedEducations"]) > 0 :
+                    for i in currentUser["selectedEducations"]:
+                        allEducations.append(i)
+
+                # Checking Blood Group
+                if len(filters["selectedBloodGroups"]) > 0:
+                    for i in filters["selectedBloodGroups"]:
+                        allBloodGroups.append(i)
+                elif len(currentUser["selectedBloodGroups"]) > 0:
+                    for i in currentUser["selectedBloodGroups"]:
+                        allBloodGroups.append(i)
+
+                # Checking Family Type
+                if len(filters["FamilyType"]) > 0:
+                    for i in filters["FamilyType"]:
+                        allFamilyTypes.append(i)
+                elif len(currentUser["selectedFamilyType"]) > 0:
+                    print(currentUser["selectedFamilyType"])
+                    for i in currentUser["selectedFamilyType"]:
+                        allFamilyTypes.append(i)
+
+                # Checking Raas
+                if len(filters["selectedRaas"]) > 0:
+                    for i in filters["selectedRaas"]:
+                        allRaas.append(i)
+                elif len(currentUser["selectedRaas"]) > 0:
+                    for i in currentUser["selectedRaas"]:
+                        allRaas.append(i)
+
+
+                if len(filters["selectedNaadi"]) > 0:
+                    for i in filters["selectedNaadi"]:
+                        allNaadi.append(i)
+                if len(currentUser["selectedNaadi"]) > 0:
+                    for i in currentUser["selectedNaadi"]:
+                        allNaadi.append(i)
+
+                if int(filters["selectedSiblingsCousinsUpto"]) > 0:
+                    newFilter["Siblings"] = {"$lte": int(filters["selectedSiblingsCousinsUpto"])}
             
-            print(newFilter)
+                # Making Filters
+                if(len(allIncomes) > 0):
+                    newFilter["IncomeGroup"] = {"$in":allIncomes}
+                if(len(allLocalities) > 0):
+                    newFilter["CurrentAddress"] = {"$in":allLocalities}
+                if len(allEducations) > 0 :
+                    newFilter["DegDip"] = {"$in":allEducations}
+                if len(allBloodGroups) > 0:
+                    newFilter["BloodGrp"] = {"$in":allBloodGroups}
+                if len(allFamilyTypes) > 0:
+                    newFilter["FamilyType"] = {"$in":allFamilyTypes}
+                if len(allRaas) > 0 :
+                    newFilter["Raas"] = {"$in": allRaas}
+                if len(allNaadi) > 0:
+                    newFilter["Naadi"] =  {"$in": allNaadi}
+                
+                print(newFilter)
 
-            total_count = collection.count_documents(newFilter) 
-          
-            data = (
-                collection.find(newFilter, projection)
-                .skip((page - 1) * rowsPerPage) 
-                .limit(rowsPerPage)  
-            )
+                total_count = collection.count_documents(newFilter) 
+            
+                data = (
+                    collection.find(newFilter, projection)
+                    .skip((page - 1) * rowsPerPage) 
+                    .limit(rowsPerPage)  
+                )
 
-            for u in data:
-                income = "Income Details Not Provided"
-                print(u["birthDate"])
-                if u["JobBis"] and u['IncomeGroup']:
-                    income = u["JobBis"] + ", earns " + u['IncomeGroup']
+                for u in data:
+                    income = "Income Details Not Provided"
+                    print(u["birthDate"])
+                    if u["JobBis"] and u['IncomeGroup']:
+                        income = u["JobBis"] + ", earns " + u['IncomeGroup']
 
-                top_data = {
-                    "Name": u['firstName'] + ' ' + u["lastName"],
-                    "Address": str(u['Address']) + ' ' + str(u["CurrentAddress"]),
-                    "Education": str(u["DegDip"]) + ', ' + str(u['Field']),
-                    "Income": income,
-                    "Userid": u['UserId'],
-                    "IsVerified":u["IsVerified"],
-                    "image": "" if (u['image'] == None)  else u['image'] ,
-                    "Birthdate": u['birthDate'],
-                    "Birthtime": u['birthTime'],
-                    "BirthPlace": u['BirthPlace'],
-                    "Bloodgroup": u["BloodGrp"] 
-                }
+                    top_data = {
+                        "Name": u['firstName'] + ' ' + u["lastName"],
+                        "Address": str(u['Address']) + ' ' + str(u["CurrentAddress"]),
+                        "Education": str(u["DegDip"]) + ', ' + str(u['Field']),
+                        "Income": income,
+                        "Userid": u['UserId'],
+                        "IsVerified":u["IsVerified"],
+                        "image": "" if (u['image'] == None)  else u['image'] ,
+                        "Birthdate": u['birthDate'],
+                        "Birthtime": u['birthTime'],
+                        "BirthPlace": u['BirthPlace'],
+                        "Bloodgroup": u["BloodGrp"] 
+                    }
 
-               
-                finaldataList.append({"topData": top_data})
-            print("finaldataList")
-            return jsonify({
-                "message": "Success",
-                "users": finaldataList,
-                "totalCount": total_count,  
-                "currentPage": page, 
-                "rowsPerPage": rowsPerPage
-            })
+                
+                    finaldataList.append({"topData": top_data})
+                print("finaldataList")
+                return jsonify({
+                    "message": "Success",
+                    "users": finaldataList,
+                    "totalCount": total_count,  
+                    "currentPage": page, 
+                    "rowsPerPage": rowsPerPage
+                })
         except Exception as e:
             print(f"Error fetching users: {e}")
             error_collection = db.get_collection('ErrorLogs')
